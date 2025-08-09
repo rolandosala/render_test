26cf5fb20a7c97797c9bcb7ab70111c03ab50226
Roland <rolandosalajr@gmail.com>
2025-08-09 21:28:41 +0800

    initial deploy

---
+++ server.js
@@ -0,0 +1,10 @@
+// Example for Express.js
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Render!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
