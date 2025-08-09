1ca23a5c92e9c9b8cca60c16bce0616499088252
Roland <rolandosalajr@gmail.com>
2025-08-09 22:12:55 +0800

    added database

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 // Example for Express.js
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const db = mysql.createConnection({
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+  port: process.env.MYSQL_ADDON_PORT
+});
+
+
+db.connect(err => {
+  if (err) throw err;
+  console.log("Connected to Clever Cloud MySQL!");
+});
 
 app.get("/", (req, res) => {
   res.send("Change by ROlando Sala");
