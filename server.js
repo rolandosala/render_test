8c3db79e8d5a02d901479c4040d3b98a5295d8f9
Roland <rolandosalajr@gmail.com>
2025-08-10 17:54:45 +0800

    fix connection

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 // Example for Express.js
 require("dotenv").config();
 const express = require("express");
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.MYSQL_ADDON_HOST,
   user: process.env.MYSQL_ADDON_USER,
   password: process.env.MYSQL_ADDON_PASSWORD,
   database: process.env.MYSQL_ADDON_DB,
-  port: process.env.MYSQL_ADDON_PORT
-});
-
-
-db.connect(err => {
-  if (err) throw err;
-  console.log("Connected to Clever Cloud MySQL!");
+  port: process.env.MYSQL_ADDON_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 app.get("/", async (req, res) => {
   try {
-    const [rows] = await db.promise().query("SELECT * FROM sample_tbl");
+    const [rows] = await db.query("SELECT * FROM sample_tbl");
     res.json(rows);
   } catch (error) {
     console.error("Error connecting to MySQL:", error);
