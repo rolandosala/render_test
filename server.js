f37eb39995c7a5b06388367dd053b91c9bc92721
Roland <rolandosalajr@gmail.com>
2025-08-12 21:06:31 +0800

    added create

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql2/promise");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
+/* app.use(cors({
+  origin: "https://your-frontend.vercel.app", // replace with your actual Vercel URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+})); */
+app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:5173",
+}));
 const db = mysql.createPool({
   host: process.env.MYSQL_ADDON_HOST,
   user: process.env.MYSQL_ADDON_USER,
@@ -26,5 +36,18 @@ app.get("/fetchdata", async (req, res) => {
   }
 
 });
+app.post('/createdata', async (req, res) => {
+  try {
+    const { firstname, lastname, birthdate } = req.body;
+    console.log("Received data:", { firstname, lastname, birthdate });
+    const [result] = await db.query("INSERT INTO sample_tbl (firstname, lastname, data) VALUES (?, ?, ?)", [firstname, lastname, birthdate]);
+    res.status(201).json({
+      id: result.insertId
+    })
+  } catch (error) {
+    console.error("Error inserting data into MySQL:", error);
+    res.status(500).json({ error: "Failed to insert data into MySQL" });
+  }
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
